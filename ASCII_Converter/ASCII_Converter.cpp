1431198ff7c29d3595c92dcde367abe749ce9877
David Hegarty <david.m.hegarty@gmail.com>
2022-04-15 20:08:00 +0100

    updated bug in looping, program requires more validation around the ASCII number inputs

--- ASCII_Converter/ASCII_Converter.cpp
+++ ASCII_Converter/ASCII_Converter.cpp
@@ -6,8 +6,11 @@
     Simple program to convert a 5 letter input to ASCII numbers, then convert a new 5 number input back to letters.
 */
 
+bool program_running = true;
 
-
+/*
+  Get user input text and output the ASCII characters 
+*/
 void convert_letters_to_ASCII() {
     char c1, c2, c3, c4, c5;
 
@@ -17,34 +20,62 @@ void convert_letters_to_ASCII() {
     std::cout << "ASCII message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << std::endl;
 }
 
+/*
+  Get user input for the ASCII characters and verify that it is an int type and below 255
+*/
+unsigned int get_ASCII_number() {
+    unsigned int integer_input;
+    std::cin >> integer_input;
+    while (std::cin.fail() && integer_input < 256) {
+        std::cout << "Input not recognized, please try again: ";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cin >> integer_input;
+    }
+    return integer_input;
+}
+
+/*
+  Collect numbers from user and oupput the ASCII characters for each.
+*/
 void convert_ASCII_to_letters() {
     unsigned int int1, int2, int3, int4, int5;
 
     std::cout << "Enter 5 ASCII numbers " << std::endl;
     std::cout << "First number: " << std::endl;
-    std::cin >> int1;
+    int1 = get_ASCII_number();
     std::cout << "Second number: " << std::endl;
-    std::cin >> int2;
+    int2 = get_ASCII_number();
     std::cout << "Third number: " << std::endl;
-    std::cin >> int3;
+    int3 = get_ASCII_number();
     std::cout << "Fourth number: " << std::endl;
-    std::cin >> int4;
+    int4 = get_ASCII_number();
     std::cout << "Fifth number: " << std::endl;
-    std::cin >> int5;
+    int5 = get_ASCII_number();
 
     std::cout << "ASCII message: " << char(int1) << char(int2) << char(int3) << char(int4) << char(int5) << std::endl;
 }
 
 
 /*
-  Bug: Entering letters causes an unbreakable loop  
+  Get user input for the menu and verify that it is an int type  
 */
 int get_menu_input() {
     int user_input;
     std::cin >> user_input;
+    while (std::cin.fail()) {
+        std::cout << "Input not recognized, please try again." << std::endl;
+        std::cout << "Enter a menu option (1-3): ";
+        std::cin.clear();
+        std::cin.ignore(256, '\n');
+        std::cin >> user_input;
+    }
     return user_input;
 }
 
+/*
+  Run the selected user menu option or ask for input again.
+*/
 void action_user_menu_option(int menu_option) {
     switch (menu_option) {
     case 1:
@@ -54,6 +85,7 @@ void action_user_menu_option(int menu_option) {
         convert_ASCII_to_letters();
         break;
     case 3:
+        program_running = false;
         exit(0);
         break;
     default:
@@ -63,6 +95,9 @@ void action_user_menu_option(int menu_option) {
     }
 }
 
+/*
+  Print out the menu and ask user for input
+*/
 void main_menu() {
     int menu_option;
     std::cout << "ASCII Converter Menu" << std::endl;
@@ -76,16 +111,13 @@ void main_menu() {
 }
 
 
-
-
-
 int main()
-{
-    
-    
-    main_menu();
-    
-
+{   
+    while (program_running)
+    {
+        main_menu();
+    }
+ 
     std::system("pause>0");
 }
 
